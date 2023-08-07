6f930b59cf219ee4c28bbd8fbf769f3d61903c20
Wesley Peters <wesleypeters@gmail.com>
2023-08-07 12:28:46 -0600

    add routes/requests to app.py, seed database

--- server/app.py
+++ server/app.py
@@ -1,23 +1,159 @@
-#!/usr/bin/env python3
+from flask import Flask, request, make_response, jsonify
+from flask_restful import Api, Resource
+from models import db, Doctor, Patient, Appointment, Login
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+import os
 
-# Standard library imports
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+DATABASE = os.environ.get(
+    "DB_URI", f"sqlite:///{os.path.join(BASE_DIR, 'app.db')}")
 
-# Remote library imports
-from flask import request
-from flask_restful import Resource
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
 
-# Local imports
-from config import app, db, api
-# Add your model imports
+migrate = Migrate(app, db)
 
+api = Api(app)
 
-# Views go here!
+db.init_app(app)
 
 @app.route('/')
 def index():
     return '<h1>Phase 4 Project Server</h1>'
 
+class Doctors(Resource):
+    def get(self, id=None):
+        if id:
+            doctor = Doctor.query.filter_by(id=id).one_or_none()
+            if doctor is None:
+                return make_response(jsonify({'error': 'Doctor not found'}), 404)
+            else:
+                return make_response(jsonify(doctor.to_dict()), 200)
+        else:
+            doctors = Doctor.query.all()
+            return make_response(jsonify([doctor.to_dict() for doctor in doctors]), 200)
+
+    def post(self):
+        new_doctor = Doctor(**request.json)
+        db.session.add(new_doctor)
+        db.session.commit()
+        return make_response(jsonify(new_doctor.to_dict()), 201)
+
+    def put(self, id):
+        doctor = Doctor.query.filter_by(id=id).one_or_none()
+        if doctor is None:
+            return make_response(jsonify({'error': 'Doctor not found'}), 404)
+        else:
+            for key, value in request.json.items():
+                setattr(doctor, key, value)
+            db.session.commit()
+            return make_response(jsonify(doctor.to_dict()), 200)
+
+    def delete(self, id):
+        doctor = Doctor.query.filter_by(id=id).one_or_none()
+        if doctor is None:
+            return make_response(jsonify({'error': 'Doctor not found'}), 404)
+        else:
+            db.session.delete(doctor)
+            db.session.commit()
+            return make_response(jsonify({'result': 'Doctor deleted'}), 200)
+
+
+class Patients(Resource):
+    def get(self, id=None):
+        if id:
+            patient = Patient.query.filter_by(id=id).one_or_none()
+            if patient is None:
+                return make_response(jsonify({'error': 'Patient not found'}), 404)
+            else:
+                return make_response(jsonify(patient.to_dict()), 200)
+        else:
+            patients = Patient.query.all()
+            return make_response(jsonify([patient.to_dict() for patient in patients]), 200)
+
+    def post(self):
+        new_patient = Patient(**request.json)
+        db.session.add(new_patient)
+        db.session.commit()
+        return make_response(jsonify(new_patient.to_dict()), 201)
+
+    def put(self, id):
+        patient = Patient.query.filter_by(id=id).one_or_none()
+        if patient is None:
+            return make_response(jsonify({'error': 'Patient not found'}), 404)
+        else:
+            for key, value in request.json.items():
+                setattr(patient, key, value)
+            db.session.commit()
+            return make_response(jsonify(patient.to_dict()), 200)
+
+    def delete(self, id):
+        patient = Patient.query.filter_by(id=id).one_or_none()
+        if patient is None:
+            return make_response(jsonify({'error': 'Patient not found'}), 404)
+        else:
+            db.session.delete(patient)
+            db.session.commit()
+            return make_response(jsonify({'result': 'Patient deleted'}), 200)
+
+
+class Appointments(Resource):
+    def get(self, id=None):
+        if id:
+            appointment = Appointment.query.filter_by(id=id).one_or_none()
+            if appointment is None:
+                return make_response(jsonify({'error': 'Appointment not found'}), 404)
+            else:
+                return make_response(jsonify(appointment.to_dict()), 200)
+        else:
+            appointments = Appointment.query.all()
+            return make_response(jsonify([appointment.to_dict() for appointment in appointments]), 200)
+
+    def post(self):
+        new_appointment = Appointment(**request.json)
+        db.session.add(new_appointment)
+        db.session.commit()
+        return make_response(jsonify(new_appointment.to_dict()), 201)
+
+    def put(self, id):
+        appointment = Appointment.query.filter_by(id=id).one_or_none()
+        if appointment is None:
+            return make_response(jsonify({'error': 'Appointment not found'}), 404)
+        else:
+            for key, value in request.json.items():
+                setattr(appointment, key, value)
+            db.session.commit()
+            return make_response(jsonify(appointment.to_dict()), 200)
+
+    def delete(self, id):
+        appointment = Appointment.query.filter_by(id=id).one_or_none()
+        if appointment is None:
+            return make_response(jsonify({'error': 'Appointment not found'}), 404)
+        else:
+            db.session.delete(appointment)
+            db.session.commit()
+            return make_response(jsonify({'result': 'Appointment deleted'}), 200)
+
+
+class Login(Resource):
+    def post(self):
+        username = request.json.get('username')
+        password = request.json.get('password')
+        login = Login.query.filter_by(username=username, password=password).one_or_none()
+        if login is None:
+            return make_response(jsonify({'error': 'Invalid username or password'}), 401)
+        else:
+            return make_response(jsonify({'message': 'Logged in successfully'}), 200)
+
+
+api.add_resource(Doctors, '/doctors', '/doctors/<int:id>')
+api.add_resource(Patients, '/patients', '/patients/<int:id>')
+api.add_resource(Appointments, '/appointments', '/appointments/<int:id>')
+api.add_resource(Login, '/login')
+
 
 if __name__ == '__main__':
-    app.run(port=5555, debug=True)
-
+    app.run(debug=True)
--- server/seed.py
+++ server/seed.py
@@ -2,16 +2,81 @@
 
 # Standard library imports
 from random import randint, choice as rc
+import datetime
 
 # Remote library imports
 from faker import Faker
 
 # Local imports
 from app import app
-from models import db
+from models import Doctor, Patient, Appointment, Login, db
+
+fake = Faker()
+
+def create_doctors(num):
+    doctors = []
+    for _ in range(num):
+        doctor = Doctor(lastname=fake.last_name())
+        doctors.append(doctor)
+    return doctors
+
+def create_patients(num):
+    patients = []
+    for _ in range(num):
+        dob = fake.date_of_birth(minimum_age=20, maximum_age=90).strftime('%m/%d/%Y')
+        patient = Patient(firstname=fake.first_name(), lastname=fake.last_name(), dob=dob)
+        patients.append(patient)
+    return patients
+
+def create_appointments(doctors, patients):
+    appointments = []
+    for doctor in doctors:
+        for _ in range(randint(1, 10)):  # Each doctor will have between 1 to 10 appointments.
+            patient = rc(patients)
+            date = fake.date_between(start_date='-1y', end_date='+1y').strftime('%m/%d/%Y')
+            time = str(datetime.time(hour=randint(0, 23), minute=randint(0, 59)))[:5]
+            appointment = Appointment(date=date, time=time, doctor_id=doctor.id, patient_id=patient.id)
+            appointments.append(appointment)
+    return appointments
+
+def create_logins(doctors):
+    logins = []
+    for doctor in doctors:
+        login = Login(username=f"{doctor.lastname}{doctor.id}", password=fake.password(), user_type="Doctor", user=f"Dr. {doctor.lastname}")
+        logins.append(login)
+    
+    # Creating some admin logins
+    for _ in range(5):  # 5 admin accounts
+        login = Login(username=fake.user_name(), password=fake.password(), user_type="Admin", user="Admin")
+        logins.append(login)
+    
+    return logins
 
 if __name__ == '__main__':
-    fake = Faker()
     with app.app_context():
         print("Starting seed...")
-        # Seed code goes here!
+
+        db.session.rollback()  # In case a previous session was interrupted
+
+        # Remove all existing data
+        Doctor.query.delete()
+        Patient.query.delete()
+        Appointment.query.delete()
+        Login.query.delete()
+
+        # Create fake data
+        doctors = create_doctors(10)
+        patients = create_patients(50)
+        appointments = create_appointments(doctors, patients)
+        logins = create_logins(doctors)
+
+        # Add fake data to the session
+        db.session.add_all(doctors)
+        db.session.add_all(patients)
+        db.session.add_all(appointments)
+        db.session.add_all(logins)
+
+        # Commit the session to store data in the database
+        db.session.commit()
+
+        print("Seeding completed successfully!")
