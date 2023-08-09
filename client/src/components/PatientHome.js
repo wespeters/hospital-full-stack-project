f139cdf5a804d2c32ac6238f5a0d2cb9e7f770b0
Wesley Peters <wesleypeters@gmail.com>
2023-08-09 14:47:26 -0600

    add functionality to Patient Home Page

--- client/src/components/PatientHome.js
+++ client/src/components/PatientHome.js
@@ -4,7 +4,8 @@ const PatientHome = () => {
   const [action, setAction] = useState(null);
   const [appointments, setAppointments] = useState([]);
   const [doctors, setDoctors] = useState([]);
-  const [newAppointment, setNewAppointment] = useState({ doctorId: "", date: "" });
+  const [newAppointment, setNewAppointment] = useState({ doctor_id: "", date: "" });
+  const [changeDate, setChangeDate] = useState(""); // Added state for change date
 
   const fetchAppointments = () => {
     fetch("/appointments") 
@@ -43,23 +44,30 @@ const PatientHome = () => {
       .then(() => fetchAppointments());
   };
 
-  const handleChangeAppointment = (appointmentId, newDate) => {
+  const handleChangeAppointment = (appointmentId) => {
+    // Convert the date from 'YYYY-MM-DD' to 'MM/DD/YYYY'
+    const convertedDate = changeDate.split('-').reverse().join('/');
+
     fetch(`/appointments/${appointmentId}`, {
       method: "PATCH",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ date: newDate }),
+      body: JSON.stringify({ date: convertedDate }),
     })
       .then(() => fetchAppointments());
   };
 
   const handleCreateAppointment = () => {
+    // Convert the date from 'YYYY-MM-DD' to 'MM/DD/YYYY'
+    const [year, month, day] = newAppointment.date.split('-');
+    const convertedDate = `${month}/${day}/${year}`;
+    
     fetch("/appointments", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(newAppointment),
+      body: JSON.stringify({ ...newAppointment, date: convertedDate }),
     })
       .then(() => fetchAppointments());
-  };
+  };  
 
   const handleNewAppointmentChange = (e) => {
     setNewAppointment({ ...newAppointment, [e.target.name]: e.target.value });
@@ -76,9 +84,18 @@ const PatientHome = () => {
       </div>
       {action === "view" && <div>{appointments.map((appointment) => <p key={appointment.id}>{appointment.date} with Dr. {appointment.doctor ? appointment.doctor.lastname : "Unknown"}</p>)}</div>}
       {action === "cancel" && <div>{appointments.map((appointment) => <p key={appointment.id}>{appointment.date} with Dr. {appointment.doctor ? appointment.doctor.lastname : "Unknown"} <button onClick={() => handleCancelAppointment(appointment.id)}>Cancel</button></p>)}</div>}
-      {action === "change" && <div>{appointments.map((appointment) => <p key={appointment.id}>{appointment.date} with Dr. {appointment.doctor ? appointment.doctor.lastname : "Unknown"} <button onClick={() => handleChangeAppointment(appointment.id, "newDate")}>Change</button></p>)}</div>}
+      {action === "change" && <div>
+        {appointments.map((appointment) => (
+          <div key={appointment.id}>
+            <p>{appointment.date} with Dr. {appointment.doctor ? appointment.doctor.lastname : "Unknown"}</p>
+            <input type="date" value={changeDate} onChange={(e) => setChangeDate(e.target.value)} />
+            <button onClick={() => handleChangeAppointment(appointment.id)}>Change</button>
+          </div>
+        ))}
+      </div>}
       {action === "create" && <div>
-        <select name="doctorId" onChange={handleNewAppointmentChange}>
+      <label>Select Doctor: </label>
+        <select name="doctor_id" onChange={handleNewAppointmentChange}>
           {doctors.map((doctor) => <option key={doctor.id} value={doctor.id}>{doctor.lastname}</option>)}
         </select>
         <input type="date" name="date" onChange={handleNewAppointmentChange} />
--- client/src/components/PatientLogin.js
+++ client/src/components/PatientLogin.js
@@ -3,39 +3,49 @@ import { useNavigate } from 'react-router-dom';
 
 const PatientLogin = () => {
     const [patients, setPatients] = useState([]);
-    const [patient, setPatient] = useState({});
+    const [patient, setPatient] = useState({ firstname: "", lastname: "", dob: "" }); // Initialize with empty strings
     const [errorMsg, setErrorMsg] = useState(null);
     const [loginMsg, setLoginMsg] = useState(null);
     const navigate = useNavigate();
-
+  
     useEffect(() => {
         fetch('/patients')
             .then(resp => resp.json())
             .then(setPatients)
     }, [])
-
+  
     function onChange(e){
         setPatient({...patient, [e.target.name]: e.target.value})
     }
-
-    function handleSubmit (e){
-        e.preventDefault()
-        let flag = 0
-
-        for (let i = 0; i < patients.length; i++){
-            if ((patients[i].dob === patient.dob) && (patients[i].firstname === patient.firstname) && (patients[i].lastname === patient.lastname)) {
-                flag=1
-            }
+  
+    function handleSubmit(e) {
+        e.preventDefault();
+        let flag = 0;
+      
+        const inputDobDate = new Date(patient.dob);
+        const inputDob = `${String(inputDobDate.getUTCMonth() + 1).padStart(2, '0')}/${String(inputDobDate.getUTCDate()).padStart(2, '0')}/${inputDobDate.getUTCFullYear()}`;
+      
+        for (let i = 0; i < patients.length; i++) {
+          console.log('Comparing:', patients[i].dob, 'with input:', inputDob);
+      
+          if (
+            patients[i].dob === inputDob &&
+            patients[i].firstname === patient.firstname &&
+            patients[i].lastname === patient.lastname
+          ) {
+            flag = 1;
+          }
         }
-
-        if (flag === 1){
-            setLoginMsg('Logged in!')
-            navigate('/patient-home')
+      
+        if (flag === 1) {
+          setLoginMsg('Logged in!');
+          navigate('/patient-home');
         } else {
-            setErrorMsg('Login unsuccessful')
+          setErrorMsg('Login unsuccessful');
         }
-    }
-
+      }
+      
+  
     return (
         <div>
             {errorMsg && <div>{errorMsg}</div>}
@@ -69,6 +79,7 @@ const PatientLogin = () => {
             </form>
         </div>
     );
-};
-
-export default PatientLogin;
+  };
+  
+  export default PatientLogin;
+  
