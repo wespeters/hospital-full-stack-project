5470882a483107872ccbbf252fcba195dd728228
Wesley Peters <wesleypeters@gmail.com>
2023-08-10 19:10:44 -0600

    small changes

--- client/src/components/DoctorHome.js
+++ client/src/components/DoctorHome.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 import DarkModeToggle from './DarkModeToggle';
 
 function DoctorHome (){
